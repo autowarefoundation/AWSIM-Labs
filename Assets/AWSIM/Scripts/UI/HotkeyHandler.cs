7da77ea5d8fc4241924f6777eaf3e8073791582a
M. Fatih Cırıt <mfc@leodrive.ai>
2024-03-12 21:07:11 +0300

    reorganize everything
    Signed-off-by: M. Fatih Cırıt <mfc@leodrive.ai>

--- Assets/AWSIM/Scripts/UI/HotkeyHandler.cs
+++ Assets/AWSIM/Scripts/UI/HotkeyHandler.cs
@@ -1,48 +1,39 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
-
-//This script will be improved in the future. Temporarily it will be a bit dirty.
-namespace AWSIM
+namespace AWSIM.Scripts.UI
 {
     public class HotkeyHandler : MonoBehaviour
     {
-        private AutowareSimulation _simulation;
-        private Transform _egoTransform;
-        private Rigidbody _egoRigidbody;
-        private Vector3 _initialEgoPosition;
-        private Quaternion _initialEgoRotation;
+        [SerializeField] private Canvas mainMenu;
 
+        private EgoVehiclePositionManager egoVehiclePositionManager;
 
-        private void Start()
+        private void Awake()
         {
-            _egoTransform = gameObject.GetComponent<AutowareSimulation>().egoTransform;
-            // Store the initial position and rotation of the ego.
-            _initialEgoPosition = _egoTransform.position;
-            _initialEgoRotation = _egoTransform.rotation;
-            // Get the rigidbody of the ego
-            _egoRigidbody = _egoTransform.GetComponent<Rigidbody>();
+            egoVehiclePositionManager = GetComponent<EgoVehiclePositionManager>();
         }
 
         void Update()
         {
-            // If the escape key is pressed, toggle the GUI panel.
+            // Toggle the main menu
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!mainMenu)
+                {
+                    Debug.LogWarning(
+                        "The mainMenu reference is null. Please ensure the mainMenu Canvas object is assigned to mainMenu in the Inspector. Without it, the GUI cannot be toggled on/off.");
+                }
+                else
+                {
+                    mainMenu.enabled = !mainMenu.enabled;
+                }
+            }
+
             if (Input.GetKeyDown(KeyCode.R))
             {
-                ResetEgoToSpawnPoint();
+                egoVehiclePositionManager.ResetEgoToSpawnPoint();
             }
-        }
-
-        // If the ego transform reference is present, reset the ego to the initial position and rotation.
-        public void ResetEgoToSpawnPoint()
-        {
-            if (!_egoTransform)
-            {
-                Debug.LogWarning("Ego transform reference is missing. No ego to reset here!");
-                return;
-            }
-
-            _egoTransform.SetPositionAndRotation(_initialEgoPosition, _initialEgoRotation);
-            _egoRigidbody.Sleep();
         }
     }
 }
