66cf584efe0c4bcfb603ae45c11c3f6dfddc1333
Alptuğ Cırıt <alptug@leodrive.ai>
2024-04-05 15:15:22 +0300

    add: AutowareLabs Logo and move logos to MainMenuCanvas
    Signed-off-by: Alptuğ Cırıt <alptug@leodrive.ai>

--- Assets/AWSIM/Scripts/UI/DemoUI.cs
+++ Assets/AWSIM/Scripts/UI/DemoUI.cs
@@ -19,7 +19,7 @@ namespace AWSIM
             timeScaleText.text = "x " + timeScaleSlider.value.ToString("F2");
             var version = Application.version;
             print(version);
-            versionText.text = "AWSIM v " + version;
+            versionText.text = "AWSIMLabs v " + version;
         }
 
         public void SetTimeScale(float timeScale)
