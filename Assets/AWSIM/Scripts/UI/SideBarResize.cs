2c8d33128fb7f8d8a6711a53f61d708e65012fbc
Alptuğ Cırıt <alptug@leodrive.ai>
2024-07-04 17:32:05 +0300

    change cursor in drag border & fix drag detection behaviour
    Signed-off-by: Alptuğ Cırıt <alptug@leodrive.ai>

--- Assets/AWSIM/Scripts/UI/SideBarResize.cs
+++ Assets/AWSIM/Scripts/UI/SideBarResize.cs
@@ -9,13 +9,21 @@ namespace AWSIM.Scripts.UI
         [SerializeField] private float _sidebarMinimumWidth = 220;
         [SerializeField] private float _sidebarDragBorderSize = 5;
         [SerializeField] private RectTransform _staticObjects;
-
+        [SerializeField] private Texture2D _cursorResizeTexture2D;
+        private readonly Vector2 _cursorResizeTexture2DHotspot = new(16, 16);
         private Vector2 _initialDragPointOffset;
         private Vector2 _initialSizeDelta;
         private RectTransform _sideBarRectTransform;
         private UISideBarHandler _sideBarHandler;
         private Image _sideBarImage;
-        private bool _dragActive;
+        private Canvas _canvas;
+
+        private float _dragAreaRightBound;
+        private float _dragAreaLeftBound;
+        private float _pointerPositionX;
+
+        private bool _isDragActive;
+        private bool _isCursorInDragArea;
 
         private void Start()
         {
@@ -23,52 +31,79 @@ namespace AWSIM.Scripts.UI
             _sideBarHandler = GetComponentInParent<UISideBarHandler>();
             _sideBarImage = GetComponent<Image>();
             _sideBarImage.raycastPadding = new Vector4(_sidebarMinimumWidth - _sidebarDragBorderSize, 0, 0, 0);
+            _canvas = _sideBarRectTransform.GetComponentInParent<Canvas>();
+        }
+
+        private void Update()
+        {
+            UpdateCursor();
+        }
+
+        private void UpdateCursor()
+        {
+            if (!_canvas) return;
+
+            var pointerPosition = Input.mousePosition;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_sideBarRectTransform, pointerPosition,
+                _canvas.worldCamera, out var localPointerPosition);
+
+            var scaleFactor = _canvas.scaleFactor;
+            _pointerPositionX = localPointerPosition.x * scaleFactor;
+            _dragAreaRightBound = _sideBarRectTransform.rect.xMax * scaleFactor;
+            _dragAreaLeftBound = (_sideBarRectTransform.rect.xMin + _sideBarImage.raycastPadding.x) * scaleFactor;
+
+            if (_dragAreaRightBound >= _pointerPositionX && _pointerPositionX >= _dragAreaLeftBound)
+            {
+                if (!_isCursorInDragArea)
+                {
+                    Cursor.SetCursor(_cursorResizeTexture2D, _cursorResizeTexture2DHotspot, CursorMode.Auto);
+                    _isCursorInDragArea = true;
+                }
+            }
+            else
+            {
+                if (_isCursorInDragArea)
+                {
+                    Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                    _isCursorInDragArea = false;
+                }
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!_isCursorInDragArea && !_isDragActive)
+            {
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                _isCursorInDragArea = false;
+            }
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            var canvas = _sideBarRectTransform.GetComponentInParent<Canvas>();
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(_sideBarRectTransform, eventData.position,
-                canvas.worldCamera, out var localPointerPosition);
-
-            var scaleFactor = canvas.scaleFactor;
-            // Calculate detection bounds with "canvas.scaleFactor"
-            var pointerPositionX = localPointerPosition.x * scaleFactor;
-            var areaRightBound = _sideBarRectTransform.rect.xMax * scaleFactor;
-            var areaLeftBound = (_sideBarRectTransform.rect.xMin + _sideBarImage.raycastPadding.x) * scaleFactor;
-
-            //// Debugs for testing
-            // Debug.DrawLine(new Vector3(_sideBarRectTransform.rect.xMax * scaleFactor, 500, 0),
-            //     new Vector3((_sideBarRectTransform.rect.xMin + _sideBarImage.raycastPadding.x) *
-            //                 scaleFactor, 500, 0), Color.red, 1000f,
-            //     false);
-            // Debug.Log(_sideBarRectTransform.rect.xMax * scaleFactor + " " + pointerPositionX + " " +
-            //           (_sideBarRectTransform.rect.xMin + _sideBarImage.raycastPadding.x) * scaleFactor);
-
-            // Check if the pointer is within the drag border
-            if (areaRightBound > pointerPositionX && pointerPositionX > areaLeftBound)
+            if (_isCursorInDragArea)
             {
-                _dragActive = true;
+                _isDragActive = true;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(_sideBarRectTransform, eventData.position,
+                    eventData.pressEventCamera, out var localPointerPosition);
+                var scaleFactor = _canvas.scaleFactor;
+                _initialDragPointOffset = new Vector2(localPointerPosition.x * scaleFactor - _pointerPositionX, 0);
+                _initialSizeDelta = _sideBarRectTransform.sizeDelta;
             }
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            if (_dragActive)
+            if (_isDragActive)
             {
-                // Lock the cursor to game window
-                Cursor.lockState = CursorLockMode.Confined;
-
-                var canvas = _sideBarRectTransform.GetComponent<Canvas>();
+                if (!_canvas) return;
 
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(_sideBarRectTransform, eventData.position,
-                    canvas.worldCamera, out var localPointerPosition);
+                    eventData.pressEventCamera, out var localPointerPosition);
 
-                _sideBarRectTransform.sizeDelta =
-                    new Vector2(localPointerPosition.x, _initialSizeDelta.y) -
-                    new Vector2(_initialDragPointOffset.x, 0);
+                _sideBarRectTransform.sizeDelta = new Vector2(localPointerPosition.x, _initialSizeDelta.y) -
+                                                  new Vector2(_initialDragPointOffset.x, 0);
 
-                // update static objects rect
                 _staticObjects.sizeDelta = new Vector2(_sideBarRectTransform.sizeDelta.x, _staticObjects.sizeDelta.y);
 
                 var padding = _sideBarImage.raycastPadding;
@@ -79,22 +114,17 @@ namespace AWSIM.Scripts.UI
             }
         }
 
-        // Reset cursor lock and drag state
         public void OnPointerUp(PointerEventData eventData)
         {
-            Cursor.lockState = CursorLockMode.None;
-            _dragActive = false;
+            _isDragActive = false;
         }
 
-        // Limit the minimum width of the sidebar
         public void OnRectTransformDimensionsChange()
         {
-            if (_sideBarRectTransform == null) return;
+            if (!_sideBarRectTransform) return;
             if (_sideBarRectTransform.sizeDelta.x < _sidebarMinimumWidth)
             {
                 _sideBarRectTransform.sizeDelta = new Vector2(_sidebarMinimumWidth, _initialSizeDelta.y);
-
-                // update static objects rect
                 _staticObjects.sizeDelta = new Vector2(_sideBarRectTransform.sizeDelta.x, _staticObjects.sizeDelta.y);
             }
         }
