45ac96b92291f1399da28f2efbb4d4bbb695b4b4
Alptuğ Cırıt <alptug@leodrive.ai>
2024-09-20 20:49:50 +0300

    update prefab
    Signed-off-by: Alptuğ Cırıt <alptug@leodrive.ai>

--- Assets/AWSIM/Scripts/UI/VehicleDashboard.cs
+++ Assets/AWSIM/Scripts/UI/VehicleDashboard.cs
@@ -1,3 +1,5 @@
+using AWSIM.Scripts.Vehicles.VPP_Integration;
+using AWSIM.Scripts.Vehicles.VPP_Integration.Enums;
 using UnityEngine;
 using UnityEngine.UI;
 using VehiclePhysics;
@@ -7,28 +9,201 @@ namespace AWSIM.Scripts.UI
     public class VehicleDashboard : MonoBehaviour
     {
         private VPVehicleController _vehicleController;
-        private Text _speedText;
-        private Text _gearText;
-        private Text _absText;
-        private Text _tcsText;
-        private Text _espText;
-        private Text _asrText;
+        private AutowareVPPAdapter _adapter;
+
+        [SerializeField] private Text _speedText;
+        [SerializeField] private Text _transmissionModeText;
+        [SerializeField] private Text _gearText;
+        [SerializeField] private Text _absText;
+        [SerializeField] private Text _tcsText;
+        [SerializeField] private Text _escText;
+        [SerializeField] private Text _asrText;
+        [SerializeField] private Text _controlModeText;
+
+        [SerializeField] private Color _systemActiveColor = Color.green;
+        [SerializeField] private Color _systemInactiveColor = Color.gray;
+
+        [SerializeField] private Color _conrtolModeAutonomousColor = Color.cyan;
+        [SerializeField] private Color _controlModeManualColor = Color.yellow;
+
+        private const float MsToKmH = 3.6f;
 
         private void Start()
         {
             _vehicleController = FindObjectOfType<VPVehicleController>();
-            _speedText = transform.Find("Speed").GetComponent<Text>();
-            _gearText = transform.Find("Gear").GetComponent<Text>();
-            _absText = transform.Find("ABS").GetComponent<Text>();
-            _tcsText = transform.Find("TCS").GetComponent<Text>();
-            _espText = transform.Find("ESP").GetComponent<Text>();
-            _asrText = transform.Find("ASR").GetComponent<Text>();
+            _adapter = FindObjectOfType<AutowareVPPAdapter>();
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            _speedText.text = _vehicleController.speed.ToString("F0");
-            // _gearText.text = _vehicleController.data.bus[][VehicleData.GearboxGear].ToString();
+            UpdateDashboard();
+        }
+
+        private void UpdateDashboard()
+        {
+            _speedText.text = UpdateSpeed(_vehicleController);
+            _transmissionModeText.text = UpdateTransmissionMode(_vehicleController);
+            _gearText.text = UpdateGear(_vehicleController);
+            UpdateAbsState(_vehicleController, _absText);
+            UpdateAsrState(_vehicleController, _asrText);
+            UpdateEscState(_vehicleController, _escText);
+            UpdateTcsState(_vehicleController, _tcsText);
+            UpdateControlMode(_adapter, _controlModeText);
+        }
+
+        private void UpdateControlMode(AutowareVPPAdapter adapter, Text text)
+        {
+            if (adapter == null)
+            {
+                text.text = "N/A";
+                text.color = _systemInactiveColor;
+            }
+            else
+            {
+                switch (adapter.ControlModeInput)
+                {
+                    case VPPControlMode.Autonomous:
+                        text.text = "Autonomous";
+                        text.color = _conrtolModeAutonomousColor;
+                        break;
+                    case VPPControlMode.Manual:
+                        text.text = "Manual";
+                        text.color = _controlModeManualColor;
+                        break;
+                    case VPPControlMode.NoCommand:
+                        text.text = "NoCommand";
+                        text.color = _systemInactiveColor;
+                        break;
+                    case VPPControlMode.AutonomousSteerOnly:
+                        text.text = "AutonomousSteerOnly";
+                        text.color = _systemInactiveColor;
+                        break;
+                    case VPPControlMode.AutonomousVelocityOnly:
+                        text.text = "AutonomousVelocityOnly";
+                        text.color = _systemInactiveColor;
+                        break;
+                    case VPPControlMode.Disengaged:
+                        text.text = "Disengaged";
+                        text.color = _systemInactiveColor;
+                        break;
+                    case VPPControlMode.NotReady:
+                        text.text = "Not Ready";
+                        text.color = _systemInactiveColor;
+                        break;
+                    default:
+                        text.text = "N/A";
+                        text.color = _systemInactiveColor;
+                        break;
+                }
+            }
+        }
+
+        private static string UpdateSpeed(VPVehicleController vehicle)
+        {
+            if (vehicle == null)
+            {
+                return "N/A";
+            }
+
+            var speedVal = (int)(vehicle.speed * MsToKmH);
+            // Always return positive
+            if (speedVal < 0)
+            {
+                speedVal = -speedVal;
+            }
+
+            string speed = speedVal.ToString("F0");
+            return speed;
+        }
+
+        private string UpdateTransmissionMode(VPVehicleController vehicle)
+        {
+            if (vehicle == null)
+            {
+                return "N/A";
+            }
+
+            int modeVal = _vehicleController.data.bus[Channel.Vehicle][VehicleData.GearboxMode];
+
+            return modeVal switch
+            {
+                0 => "Manual",
+                1 => "Park",
+                2 => "Reverse",
+                3 => "Neutral",
+                4 => "Auto",
+                5 => "Auto 1",
+                6 => "Auto 2",
+                7 => "Auto 3",
+                8 => "Auto 4",
+                9 => "Auto 5",
+                _ => "N/A"
+            };
+        }
+
+        private string UpdateGear(VPVehicleController vehicle)
+        {
+            if (vehicle == null)
+            {
+                return "N/A";
+            }
+
+            int gearVal = _vehicleController.data.bus[Channel.Vehicle][VehicleData.GearboxGear];
+
+            return gearVal switch
+            {
+                < 0 => "R",
+                0 => "N",
+                > 0 => gearVal.ToString()
+            };
+        }
+
+        private void UpdateAbsState(VPVehicleController vehicle, Text text)
+        {
+            if (vehicle.data.bus[Channel.Vehicle][VehicleData.AbsEngaged] != 0)
+            {
+                text.color = _systemActiveColor;
+            }
+            else
+            {
+                text.color = _systemInactiveColor;
+            }
+        }
+
+        private void UpdateAsrState(VPVehicleController vehicle, Text text)
+        {
+            if (vehicle.data.bus[Channel.Vehicle][VehicleData.AsrEngaged] != 0)
+            {
+                text.color = _systemActiveColor;
+            }
+            else
+            {
+                text.color = _systemInactiveColor;
+            }
+        }
+
+        private void UpdateEscState(VPVehicleController vehicle, Text text)
+        {
+            if (vehicle.data.bus[Channel.Vehicle][VehicleData.EscEngaged] != 0)
+            {
+                text.color = _systemActiveColor;
+            }
+            else
+            {
+                text.color = _systemInactiveColor;
+            }
+        }
+
+        private void UpdateTcsState(VPVehicleController vehicle, Text text)
+        {
+            if (vehicle.data.bus[Channel.Vehicle][VehicleData.TcsEngaged] != 0)
+            {
+                text.color = _systemActiveColor;
+            }
+            else
+            {
+                text.color = _systemInactiveColor;
+            }
         }
     }
 }
