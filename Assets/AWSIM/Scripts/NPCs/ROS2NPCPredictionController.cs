7c6089c7c963d116585c7a88ba12c2f85840ac96
kazushi67 <osushi.115667@gmail.com>
2025-04-06 17:38:09 +0900

    refactor: change variables name
    Signed-off-by: kazushi67 <osushi.115667@gmail.com>

--- Assets/AWSIM/Scripts/NPCs/ROS2NPCPredictionController.cs
+++ Assets/AWSIM/Scripts/NPCs/ROS2NPCPredictionController.cs
@@ -71,15 +71,15 @@ namespace AWSIM
                     var velocity = (endPosition - startPosition) / (float)(predictionPointDeltaTime);
                     npcVehicle.outerSpeed = Vector3.Dot(velocity, Vector3.forward);
                     if(end_step >= 2){
-                        var prevprevPosition = ROS2Utility.RosMGRSToUnityPosition(objects[i].Kinematics.Predicted_paths[maxindex].Path[first_step-1].Position);
-                        var prevVelocity = (startPosition - prevprevPosition) / (float)(predictionPointDeltaTime);
+                        var prevPosition = ROS2Utility.RosMGRSToUnityPosition(objects[i].Kinematics.Predicted_paths[maxindex].Path[first_step-1].Position);
+                        var prevVelocity = (startPosition - prevPosition) / (float)(predictionPointDeltaTime);
                         var prevSpeed = Vector3.Dot(prevVelocity, Vector3.forward);
                         npcVehicle.outerAcceleration = (npcVehicle.outerSpeed - prevSpeed)/ (float)(predictionPointDeltaTime);
                     }
                     else
                     {
-                        var nextnextPosition = ROS2Utility.RosMGRSToUnityPosition(objects[i].Kinematics.Predicted_paths[maxindex].Path[end_step+1].Position);
-                        var nextVelocity = (nextnextPosition - endPosition) / (float)(predictionPointDeltaTime);
+                        var nextPosition = ROS2Utility.RosMGRSToUnityPosition(objects[i].Kinematics.Predicted_paths[maxindex].Path[end_step+1].Position);
+                        var nextVelocity = (nextPosition - endPosition) / (float)(predictionPointDeltaTime);
                         var nextSpeed = Vector3.Dot(nextVelocity, Vector3.forward);
                         npcVehicle.outerAcceleration = (nextSpeed - npcVehicle.outerSpeed) / (float)(predictionPointDeltaTime);  
                     }
